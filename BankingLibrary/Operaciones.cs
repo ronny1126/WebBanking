4dba5ffc12c2899fda1aa6b54ef7e2e2aba3dfd7
Eduardo Griffith <egriffith@ybpanama.com.pa>
2023-01-06 15:58:57 -0500

    Funcionalidad para contar los movimientos

--- BankingLibrary/Operaciones.cs
+++ BankingLibrary/Operaciones.cs
@@ -9,19 +9,23 @@ namespace BankingLibrary
         public static string cuenta { get; set; }
         public static double? saldo { get; set; }
         public static bool abierta { get; set; } = false;
+        public static int? movimientos { get; set; }
         public static void AperturaCuenta()
         {
             cuenta = "1001";
             saldo = 0;
             abierta = true;
+            movimientos = 0;
         }
         public static void Deposito(double valor)
         {
             saldo += valor;
+            movimientos++;
         }
         public static void Retiro(double valor)
         {
             saldo -= valor;
+            movimientos++;
         }
     }
 }
